cbdeb41077c8f5aa432128fda0df2a54b6d19c96
pyramid500 <daan.woeltgens@gmail.com>
2016-12-03 16:58:14 +0100

    Basic Menu Toolbar
    Added a basic menu toolbar with a share button and up button in the
    info page, and name only toolbar in list

--- app/src/main/java/zep/daan/myapplication/InfoPage.java
+++ app/src/main/java/zep/daan/myapplication/InfoPage.java
@@ -1,16 +1,34 @@
 package zep.daan.myapplication;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.widget.TextView;
 
 public class InfoPage extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_page);
+
+
+        // my_child_toolbar is defined in the layout file
+        Toolbar myChildToolbar =
+                (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myChildToolbar);
+
+        // Get a support ActionBar corresponding to this toolbar
+        ActionBar ab = getSupportActionBar();
+
+        // Enable the Up button
+        ab.setDisplayHomeAsUpEnabled(true);
+
+
+
         Intent infointent = getIntent();
         String message = infointent.getStringExtra("name");
         String citystate = infointent.getStringExtra("cs");
@@ -21,5 +39,11 @@ public class InfoPage extends AppCompatActivity {
         text1.setText(citystate);
         TextView text2 = (TextView) findViewById(R.id.phonenr);
         text2.setText(phonenr);
+    }
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_article, menu);
+        return true;
     }
 }
--- app/src/main/java/zep/daan/myapplication/listActivity.java
+++ app/src/main/java/zep/daan/myapplication/listActivity.java
@@ -1,8 +1,10 @@
 package zep.daan.myapplication;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -10,11 +12,21 @@ import android.widget.ListView;
 
 import java.util.ArrayList;
 
-public class listActivity extends Activity {
+public class listActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        // my_child_toolbar is defined in the layout file
+        Toolbar myChildToolbar =
+                (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myChildToolbar);
+        // Get a support ActionBar corresponding to this toolbar
+        ActionBar ab = getSupportActionBar();
+
+
+
 
         ArrayList<myArray> searchResults = GetSearchResults();
 
