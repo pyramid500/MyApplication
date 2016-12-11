9bfbb382336b38f3c1959aa325649d99b23714ba
pyramid500 <daan.woeltgens@gmail.com>
2016-12-11 14:07:29 +0100

    Search Function
    The Search Function now works, and searches for items

---
+++ app/src/main/java/zep/daan/myapplication/SearchListAdapter.java
@@ -0,0 +1,96 @@
+package zep.daan.myapplication;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.joda.time.DateTime;
+import org.joda.time.Days;
+import org.joda.time.Hours;
+import org.joda.time.format.DateTimeFormat;
+
+import java.util.ArrayList;
+
+import static zep.daan.myapplication.R.id.imageView;
+
+public class SearchListAdapter extends android.widget.BaseAdapter {
+
+    private static ArrayList<myArray> searchArrayList;
+
+    private LayoutInflater mInflater;
+
+    public SearchListAdapter(Context context, ArrayList<myArray> results) {
+        searchArrayList = results;
+        mInflater = LayoutInflater.from(context);
+    }
+
+    public int getCount() {
+        return searchArrayList.size();
+    }
+
+    public Object getItem(int position) {
+        return searchArrayList.get(position);
+    }
+
+    public long getItemId(int position) {
+        return position;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.list_item, null);
+            holder = new ViewHolder();
+            holder.txtName = (TextView) convertView.findViewById(R.id.name);
+            holder.image = (ImageView) convertView.findViewById(imageView);
+            holder.time = (TextView) convertView.findViewById(R.id.time);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+
+
+
+        String someDate = searchArrayList.get(position).getDate();
+
+        String dateResult;
+            DateTime dateTimearticle = DateTime.parse(someDate, DateTimeFormat.forPattern("dd-MM-yyyy kk:mm"));
+            DateTime presentTime = DateTime.now();
+            dateResult ="Error";
+            int days = Days.daysBetween(dateTimearticle, presentTime).getDays();
+            if(days>2) {
+                dateResult = someDate;
+            }
+            else if(days == 2){
+                dateResult = "Eergisteren";
+            }
+            else if(days == 1){
+                dateResult = "Gisteren";
+            }
+            else if (days<1){
+                int hours = Hours.hoursBetween(dateTimearticle, presentTime).getHours();
+                dateResult = Integer.toString(hours) + " Uur Geleden";
+            }
+
+
+
+
+
+
+        holder.txtName.setText(searchArrayList.get(position).getName());
+        holder.image.setImageResource(searchArrayList.get(position).getImage());
+        holder.time.setText(dateResult);
+        return convertView;
+    }
+
+    static class ViewHolder {
+        TextView txtName;
+        TextView time;
+        ImageView image;
+
+    }
+}
--- app/src/main/java/zep/daan/myapplication/SearchResults.java
+++ app/src/main/java/zep/daan/myapplication/SearchResults.java
@@ -4,7 +4,9 @@ import android.app.Activity;
 import android.app.SearchManager;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class SearchResults extends Activity {
 
@@ -14,14 +16,30 @@ public class SearchResults extends Activity {
         setContentView(R.layout.activity_search_results);
 
         // Get the intent, verify the action and get the query
-        Intent intent = getIntent();
+        Intent intent = getIntent().setData(null);
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
             String query = intent.getStringExtra(SearchManager.QUERY);
-            doMySearch(query);
-        }
-    }
+            ArrayList<myArray> searchResults2 = myArray.GetSearchResults();
+            ArrayList<myArray> results = new ArrayList<myArray>();
+            for(myArray d : searchResults2){
+                if(d.getName().contains(query) || d.getCityState().contains(query) || d.getPhone().contains(query)){
+                    myArray sr = new myArray();
+                    sr.setName(d.getName());
+                    sr.setCityState(d.getCityState());
+                    sr.setPhone(d.getPhone());
+                    sr.setDate(d.getDate());
+                    sr.setImage(d.getImage());
+                    results.add(sr);
+                }
+                if(results != null) {
+                    ListView listView1 = (ListView) findViewById(R.id.search_list);
+                    listView1.setAdapter(new SearchListAdapter(this, results));
+                }
+                else{
 
-    private void doMySearch(String query) {
-        Log.d("Event",query);
+                }
+            }
+
+        }
     }
 }
